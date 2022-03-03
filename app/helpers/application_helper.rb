6bd015a0054136e55305e00d27f0a97b5e44a6c7
galeno <gui.galenocover@gmail.com>
2022-03-03 15:08:20 -0300

    i18n - translate page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def locale(locale)
-      I18n.locale == :en ? "Estados Unidos" : "Português do Brasil" 
+      I18n.locale === :en ? "Estados Unidos" : "Português do Brasil" 
     end
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
