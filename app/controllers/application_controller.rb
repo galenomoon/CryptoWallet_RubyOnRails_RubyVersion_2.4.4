31b6ec72998c74cdc2033292f855f4cfc54cd76c
galeno <gui.galenocover@gmail.com>
2022-03-04 09:46:52 -0300

    i18n | Option En/Pt-BR in Frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base #global controller
+    before_action :set_locale
+    def set_locale
+        if params[:locale] #pega o o idioma pelo parâmetro na URL
+            cookies[:locale] = params[:locale]
+        end
+        
+        if cookies[:locale] #pega o idioma pelo cookie
+            if I18n.locale != cookies[:locale]
+            I18n.locale = cookies[:locale]      
+            end  
+        end
+    end
 end
